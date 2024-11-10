908f3f8fa72e52730d9c50a88d3a5feacb1c9d96
davidfdesousa <davidfdesousa@hotmail.com>
2024-11-10 19:45:45 -0300

    create page singIn

--- src/app/SignIn/page.tsx
+++ src/app/SignIn/page.tsx
@@ -1,9 +1,53 @@
 import React from "react";
 
-export default function SignIn () {
-    return (
-        <div>
-            <h1>SignIn</h1>
+import "./styles.css";
+
+export default function SignIn() {
+  return (
+    <article>
+      <section className="smc-container">
+        <div className="container mx-auto">
+          <h1 className="text-2xl mb-4 pl-4">Sign In</h1>
+          <form className="max-w-sm mx-auto">
+            <div className="mb-5">
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="name@flowbite.com"
+                required
+              />
+            </div>
+            <div className="mb-5">
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+          </form>
         </div>
-    )
+      </section>
+    </article>
+  );
 }

573ee4a1c102af7aa2f25ca9f5013614ac8e30e4
davidfdesousa <davidfdesousa@hotmail.com>
2024-11-10 21:15:33 -0300

    adjust routes

--- src/app/SignIn/page.tsx
+++ src/app/SignIn/page.tsx
@@ -7,51 +7,66 @@ export default function SignIn() {
   return (
     <article>
       <section className="smc-container">
-        <div className="container mx-auto">
-          <h1 className="text-2xl mb-4 pl-4">Entrar</h1>
-          <form className="max-w-sm mx-auto">
-            <div className="mb-5">
+        <div className="w-full lg:max-w-xl p-4 ">
+          <h2 className="text-2xl font-medium text-gray-900 dark:text-white">
+            Entrar
+          </h2>
+          <form className="mt-8 space-y-6" action="#">
+            <div>
               <label
-                htmlFor="email"
+                for-html="email"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Email
               </label>
               <input
                 type="email"
+                name="email"
                 id="email"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="name@flowbite.com"
+                placeholder="name@company.com"
                 required
               />
             </div>
-            <div className="mb-5">
+            <div>
               <label
-                htmlFor="password"
+                for-html="password"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Senha
               </label>
               <input
                 type="password"
+                name="password"
                 id="password"
+                placeholder="••••••••"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 required
               />
             </div>
-
+            <div>
+              <Link href={"#"}>
+                <p className="ms-auto text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+                  Esqueceu a senha?
+                </p>
+              </Link>
+            </div>
             <button
               type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mb-4"
+              className="w-full px-4 py-2 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Submit
+              Entrar
             </button>
-
-            <Link href={"#"}>
-              <p className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                Esqueci minha senha
+            <div className="text-sm font-medium text-gray-900 dark:text-white">
+              <p>
+                Ainda não tem uma conta?{" "}
+                <Link href={"/SignUp"}>
+                  <span className="text-blue-600 hover:underline dark:text-blue-500">
+                    Criar conta
+                  </span>
+                </Link>
               </p>
-            </Link>
+            </div>
           </form>
         </div>
       </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,19 +14,19 @@ export default function Home() {
             alignItems: "center",
           }}
         >
-          <Link href={"/SignIn"} className="text-cyan-50">
+          <Link href={"/SignUp"} className="text-cyan-50">
             <button
               type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
             >
               Login
             </button>
           </Link>
 
-          <Link href={"/SignUp"} className="text-cyan-50">
+          <Link href={"/SignIn"} className="text-cyan-50">
             <button
               type="button"
-              className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
             >
               Registrar-se
             </button>
